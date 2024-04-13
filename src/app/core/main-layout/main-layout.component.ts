700804dc83af641bda9e82359c9854ee5adac702
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-04-12 23:23:16 -0300

    responsiveMasonry

--- src/app/core/main-layout/main-layout.component.ts
+++ src/app/core/main-layout/main-layout.component.ts
@@ -6,6 +6,7 @@ import { FormsModule, NgModel } from '@angular/forms';
 import { GameSearchService } from '../services/common/game-search.service';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { AsideBarComponent } from './aside-bar/aside-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
@@ -14,10 +15,9 @@ import { AsideBarComponent } from './aside-bar/aside-bar.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [AutoDestroyService],
   standalone: true,
-  imports: [RouterOutlet, FormsModule, TopBarComponent, AsideBarComponent],
+  imports: [RouterOutlet, FormsModule, TopBarComponent, AsideBarComponent, CommonModule],
 })
 export class MainLayoutComponent {
-
 
 
 }
