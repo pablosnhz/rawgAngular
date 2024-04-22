0bb1ea968c48d5d9bcd31a968c632da1e25d2a1a
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-04-22 19:11:06 -0300

    loginUser

---
+++ src/app/core/services/common/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, WritableSignal, signal } from '@angular/core';
+import { Observable, of, finalize, delay } from 'rxjs';
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  $loading: WritableSignal<boolean> = signal(false);
+
+  constructor(  ) { }
+
+  login({email, password, rememberMe}: {email: string, password: string, rememberMe: boolean}): Observable<User>  {
+    this.$loading.set(true);
+    // console.log('login', email, password, rememberMe);
+    return of( { email, name: 'Emesse Enne' })
+    .pipe(
+      delay((1500)),
+      finalize(() => this.$loading.set(false))
+
+    )
+  }
+}
+
--- src/app/routes/pages/games-page/auth/pages/login-page/login-page.component.ts
+++ src/app/routes/pages/games-page/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { object } from 'firebase-functions/v1/storage';
+import { Subject, exhaustMap, filter, of, takeUntil, tap } from 'rxjs';
+import { AutoDestroyService } from 'src/app/core/services/utils/auto-destroy.service';
+import { AuthService } from '../../../../../../core/services/common/auth.service';
+import { User } from 'src/app/core/models/user';
+import { SpinnerComponent } from 'src/app/shared/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
-  imports: [],
-  standalone: true
+  imports: [ReactiveFormsModule, SpinnerComponent, CommonModule],
+  providers: [AutoDestroyService],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
+
+  form: FormGroup;
+  submitClicked$: Subject<void> = new Subject<void>();
+  rememberMe: boolean = false;
+  $loading: Signal<boolean> = this.authService.$loading
+
+  constructor( private fb: FormBuilder, private destroy$: AutoDestroyService, private authService: AuthService ){}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.subscribeToSubmit();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      rememberMe: [false]
+    })
+  }
+
+  subscribeToSubmit(): void {
+    this.submitClicked$
+    .pipe(takeUntil(this.destroy$))
+    .pipe(
+      tap(() => {
+        if(this.form.invalid){
+          Object.values(this.form.controls).forEach(control => {
+            if(control.invalid){
+              control.markAsDirty();
+              control.updateValueAndValidity({ onlySelf: true })
+            }
+          })
+        }
+      }),
+      filter(() => this.form.valid),
+      exhaustMap(() =>
+        this.authService.login( this.form.value ))
+    )
+    .subscribe((user: User) => {
+      console.log('user', user);
+    })
+  }
 
 }
